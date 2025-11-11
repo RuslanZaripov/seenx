f81915dc79d1ff75d4a52fdeb28d6d3a2bb042a8
Руслан Зарипов <rzaripov@MacBook-Air-Ruslan.local>
2025-11-11 14:27:53 +0300

    add sound_features.py

---
+++ src/sound_features.py
@@ -0,0 +1,52 @@
+import librosa
+import numpy as np
+import pandas as pd
+from IPython.display import Audio
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from logger import Logger
+
+logger = Logger(show=True).get_logger('seenx')
+
+
+def sound_features_pipeline(audio_file_path, fps=24):
+    y, sr = librosa.load(audio_file_path, sr=None)
+    print(f"Audio shape: {y.shape}, Sample rate: {sr}")
+
+    frame_length = sr // fps
+    hop_length = sr // fps
+
+    # Root Mean Square (RMS) Energy: A measure of the signal’s loudness over time.
+    rms = librosa.feature.rms(
+        y=y, 
+        frame_length=frame_length,
+        hop_length=hop_length
+    )
+
+    # Zero Crossing Rate (ZCR): A measure of the frequency of sign changes in the signal.
+    zcr = librosa.feature.zero_crossing_rate(
+        y=y, 
+        frame_length=frame_length, 
+        hop_length=hop_length
+    )
+
+    centroid = librosa.feature.spectral_centroid(
+        y=y, 
+        sr=sr, 
+        hop_length=hop_length
+    )
+
+    rolloff = librosa.feature.spectral_rolloff(
+        y=y, 
+        sr=sr, 
+        hop_length=hop_length
+    )
+
+    # Combine features into a DataFrame
+    features = pd.DataFrame({
+        'rms': rms.flatten(),
+        'zcr': zcr.flatten(),
+        'centroid': centroid.flatten(),
+        'rolloff': rolloff.flatten()
+    })
+    return features
